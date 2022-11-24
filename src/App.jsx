b1cbd9c0ab71603c70887952f80ee5035196f1e9
Mario Joel Olano Ramirez <exmilder@gmail.com>
2022-11-23 20:50:49 -0500

    css

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ function App() {
   return (
     <>
       <div className='botonera'>
-        <button className='myButton'>GRUPOS</button>
-        <button className='myButton'>OCTAVOS</button>
-        <button className='myButton'>CUARTOS</button>
-        <button className='myButton'>SEMIS - FINAL</button>
+        <button className='myButton' onClick={()=>{alert('En Creacion -> No seas SAPO')}}>GRUPOS</button>
+        <button className='myButton' onClick={()=>{alert('En Creacion -> No seas SAPO')}}>OCTAVOS</button>
+        <button className='myButton' onClick={()=>{alert('En Creacion -> No seas SAPO')}}>CUARTOS</button>
+        <button className='myButton' onClick={()=>{alert('En Creacion -> No seas SAPO')}}>SEMIS - FINAL</button>
       </div>
       <div className="container">
           <GruposList />
